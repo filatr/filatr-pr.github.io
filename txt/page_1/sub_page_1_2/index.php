f5d0723d52ba221b6c8ed2138f4bddf7abd5b38c
filatr <47829879+filatr@users.noreply.github.com>
2019-03-05 16:51:16 +0200

    Update index.php

--- txt/page_1/sub_page_1_2/index.php
+++ txt/page_1/sub_page_1_2/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
-		<title>Проработаные страницы</title>
+		<title>Проработаные страницы 2</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="description"  content="Песочница. Просто песочница. Проходи дальше" />
 		<meta name="robots" content="index, follow">
