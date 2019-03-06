417169ab7339d19ffccfedee1a5ddb1d5ec2af79
filatr <host1gb@gmail.com>
2019-03-06 23:03:51 +0200

    1111

---
+++ github/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Песочница. github</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="index, follow">
+<meta name="description" content="Песочница с ссылочками" />
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/scripts.php"); ?>
+</head>
+<body>
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/header.php"); ?>
+<div class="contet">
+<h1>Заметки о github</h1>
+
+<p>Раздел в процессе наполнения</p>
+
+</div>
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/footer.php"); ?>
+</body>
+</html>
