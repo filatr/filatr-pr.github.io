3e2fc34eb9639ba6d57a2c01c30eb8b3dd61a217
filatr <host1gb@gmail.com>
2019-03-27 20:10:47 +0200

    1111

---
+++ txt/4js/next.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+		<title>next</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="description"  content="Простейший калькулятор" />
+		<meta name="robots" content="index, follow">
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/scripts.php"); ?>
+</head>
+<body>
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/header.php"); ?>
+<div class="contet">
+<h1>Занятие от 27 03</h1>
+<p>
+<script>
+
+
+</script>
+</p>
+
+
+
+
+</div>
+<?php include ("{$_SERVER['DOCUMENT_ROOT']}/template/inc/footer.php"); ?>
+</body>
+</html>
